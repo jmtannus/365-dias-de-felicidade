756259f27922baa31706ab75128ccd4e3e41bb91
Janainateixeira <jmtannus@gmail.com>
2022-10-13 08:13:56 -0300

    tentando consertar os erros. :/

---
+++ 365-TelaInicial/src/mocks/textos.js
@@ -0,0 +1,32 @@
+import base from '../../assets/base.png';
+import logo from '../../assets/logo.png';
+import topo from '../../assets/topo.png';
+
+import tela2 from '../../assets/tela2.png';
+
+const textos = {
+  //primeira tela
+  topo: {
+    titulo: "1ªpg AV1 22.2 JanaT²",
+    logo: logo,
+    topo: topo,
+  },
+  detalhes: {
+    subtitulo: "O que te fez feliz hoje?",
+    login:"LOGIN",
+    senha: "SENHA",
+    botao: "entrar",
+    base: base,
+  },
+  //segunda tela
+  topo2: {
+    titulo: "2ªpg AV1 22.2 JanaT²",
+    background: tela2,
+  }, 
+  detalhes2: {
+    subtitulo:":) Descreva o que te fez sorrir hoje: :)",
+    botaoSalvar: "Salvar",
+  },
+}
+
+export default textos;
--- 365-TelaInicial/src/telas/SegundaTela/index.js
+++ 365-TelaInicial/src/telas/SegundaTela/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import TelaEscrita from "./componentes/TelaEscrita";
 
-import Topo from './componentes/Topo';
-import Detalhes from './componentes/Detalhes';
-
-import mock from '../../mocks/primeiraTela';
+import mock from '../../mocks/textos';
 
 export default function SegundaTela() {
   return <>
-    <Topo {...mock.topo} />
+    <TelaEscrita {...mock.textos} />
     <View style={estilos.inicial}>
-      <Detalhes {...mock.detalhes} />
+      <TelaEscrita {...mock.textos} />
     </View>
   </>
 }
