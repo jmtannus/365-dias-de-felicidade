84a96e574b9ceb2aa20fc6b2e77dad94fd00cc55
jmtannus@gmail.com <jmtannus@gmail.com>
2022-11-25 17:19:47 -0300

    Removendo o cabeçalho da página principal

--- 365-TelaInicial/App.js
+++ 365-TelaInicial/App.js
@@ -27,10 +27,10 @@ export default function App() {
   } return (
     <NavigationContainer>
         <Stack.Navigator defaultScreenOptions={SegundaTela}>
-          <Stack.Screen name="PrimeiraTela" component={PrimeiraTela} options={{ title: "Bem vindo!"}} />
-          <Stack.Screen name="SegundaTela" component={SegundaTela}/>
-          <Stack.Screen name="TerceiraTela" component={TerceiraTela}/>
+          <Stack.Screen name="PrimeiraTela" component={PrimeiraTela} options={{headerShown: false}} />
+          <Stack.Screen name="SegundaTela" component={SegundaTela} options={{headerTransparent: true}} />
+          <Stack.Screen name="TerceiraTela" component={TerceiraTela} options={{headerTransparent: true}} />
         </Stack.Navigator>
     </NavigationContainer>
-  );
+  );
 };
